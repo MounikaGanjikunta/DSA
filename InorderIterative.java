51ad6e9eb54d094d84838d90a4b14e68dba9f255
MounikaGanjikunta <89488882+MounikaGanjikunta@users.noreply.github.com>
2023-10-10 20:45:00 -0400

    Create InorderIterative.java

---
+++ InorderIterative.java
@@ -0,0 +1,61 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+            
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            
+            return newNode;
+            
+        }
+    }
+    
+    public static ArrayList<Integer> Inorder(Node root){
+        ArrayList<Integer> list = new ArrayList<>();
+        Node curr = root;
+        Stack<Node> st = new Stack<Node>();
+        while(true){
+            if(curr!= null){
+                st.push(curr);
+                curr = curr.left;
+            }
+            else{
+                if(st.isEmpty()) break;
+                
+                curr = st.pop();
+                list.add(curr.data);
+                curr = curr.right;
+            }
+        }
+        return list;
+    }
+    
+    
+    public static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+       BinaryTree bt = new BinaryTree();
+       Node root = bt.buildTree(nodes);
+       System.out.println(Inorder(root));
+    }
+}
